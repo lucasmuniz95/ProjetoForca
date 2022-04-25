1bac7665442924a32a69bb29a6697a8d08b86f4b
Lucas Muniz <andradelucasmuniz@gmail.com>
2022-04-25 18:04:36 -0300

    att 2

--- projetoforca.py
+++ projetoforca.py
@@ -8,10 +8,8 @@ letrasDigitadas = []
 letrasErradas = []
 letrasAcertadas = []
 alfabeto = list("abdcefghijklmnopqrstuvwxyz")
-<<<<<<< HEAD
 
-=======
->>>>>>> cd8e27729b5338dcd57e662043e6cf540f66a258
+
 def menu():
 	print('''
 	 >>>>>>>>>>>>         Uniesp         >>>>>>>>>>>>>>
@@ -44,13 +42,12 @@ categoria = input('Por favor, escolha sua categoria: ')
 palavrasFrutas = (random.choice(palavrasFrutas))
 palavrasCores = (random.choice(palavrasCores))
 palavrasAnimais = (random.choice(palavrasAnimais))
-<<<<<<< HEAD
-=======
+
 
 
 
 print("A palavra contem essas letras abaixo:")
->>>>>>> cd8e27729b5338dcd57e662043e6cf540f66a258
+
 
 print("\nA palavra é:")
 
@@ -72,13 +69,13 @@ for letra in palavrasFrutas:
 		break;
 
 
-<<<<<<< HEAD
+
 print(f"\n{palavra}")
-=======
+
 
 
 
 print("A palavra é:", palavra)
->>>>>>> cd8e27729b5338dcd57e662043e6cf540f66a258
+
 
 
